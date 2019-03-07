74afad11fd2597422cd56579f6a958420a464c17
Jean de Leeuw <jeandeleeuw@gmail.com>
2019-03-07 16:57:48 +0100

    Remove some example code

--- src/main/java/sdk/main/ExperimentEnvironment.java
+++ src/main/java/sdk/main/ExperimentEnvironment.java
@@ -4,6 +4,6 @@ public class ExperimentEnvironment {
 
     //This is the entrypoint to your solution.
     public void solve() {
-        System.out.println(new Example(2003546774).distanceBetween(1130606, 1130522));
+
     }
 }
